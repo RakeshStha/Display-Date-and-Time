133ce1bde8f6c529c57afa7e02baa3e2e1238e43
Rakesh Shrestha <rakeshshrestha3222@gmail.com>
2021-05-13 08:18:08 +0545

    Final sourcecode

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 var d = new Date();
 var days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
 var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-document.getElementById("demo").innerHTML = days[d.getDay()] + ", " + months[d.getMonth()] + " " + d.getDate() + ", " + d.getFullYear() ;
+document.getElementById("date").innerHTML = days[d.getDay()] + ", " + months[d.getMonth()] + " " + d.getDate() + ", " + d.getFullYear() ;
 
 setInterval(showTime, 1000);
 function showTime() {
@@ -9,7 +9,7 @@ function showTime() {
     let hour = time.getHours();
     let min = time.getMinutes();
     let sec = time.getSeconds();
-    let mil = time.Miliseconds();
+    // let mil = time.Miliseconds();
     am_pm = "AM";
   
     if (hour > 12) {
@@ -24,10 +24,10 @@ function showTime() {
     hour = hour < 10 ? "0" + hour : hour;
     min = min < 10 ? "0" + min : min;
     sec = sec < 10 ? "0" + sec : sec;
-    mil = mil < 10 ? "000" + mil : mil;
+    // mil = mil < 10 ? "000" + mil : mil;
   
     let currentTime = hour + ":" 
-            + min + ":" + sec + ":" + mil + " " + am_pm;
+             + min + ":" + sec +  " " + am_pm; //":" + mil +
   
     document.getElementById("clock")
             .innerHTML = currentTime;
